4d40a1232a4feb382ffc33ee344a1a2b16dfe9b9
Matteo308 <anellimatteo07@gmail.com>
2025-01-20 11:55:22 +0100

    titolo

--- Applicazione.java
+++ Applicazione.java
@@ -3,14 +3,14 @@ import java.awt.*;
 
 public class Applicazione {
     public static void main(String[] args) {
-        F1 nuova = new F1(null);
+        F1 nuova = new F1("Esercitazione");
         nuova.setVisible(true);
         nuova.setLocation(300,400);
         nuova.setSize(null);
 
         MainFinestra2 nuove = new MainFinestra2(null);
         //nuove.setVisible(true);
-        //nuove.setLocation(300,400);
+        //nuove.setLocation(900,400);
         //nuove.setSize(null);
     }
 }
